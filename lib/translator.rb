8c87d2fed26eaa15badb1bb5f6938f82fb242103
Brandon Teich <brandonteich123@gmail.com>
2018-10-17 23:15:11 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,7 +11,12 @@ def load_library(path)
     }
   library.each do |meaning, em_arr|
     
-    
+    em_arr e.g. [">:-(", "ヽ(ｏ`皿′ｏ)ﾉ"]
+    em_lib['get_meaning'][em_arr[1]] = meaning
+    em_lib['get_emoticon'][em_arr[0]] = em_arr[1]
+  end
+  em_lib
+end
     
     
 def get_japanese_emoticon
